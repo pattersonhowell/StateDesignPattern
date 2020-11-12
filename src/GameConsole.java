2659080856da2913dbf6c853c3b00d31f294e7c8
Patterson Howell <pattersonhowell@gmail.com>
2020-11-12 16:08:48 -0500

    game console setup

---
+++ src/GameConsole.java
@@ -0,0 +1,49 @@
+
+public class GameConsole {
+
+	private State HomeState;
+	private State NintendoState;
+	private State xBoxState;
+	private State state;
+	
+	
+	public GameConsole() {
+		
+	}
+	
+	
+	public void pressHomeButton() {
+		
+	}
+	
+	public void pressNintendoButton() {
+		
+	}
+	
+	public void pressXBoxButton() {
+		
+	}
+	
+	public void pressGameButton() {
+		
+	}
+	
+	public void setState(State state) {
+		
+	}
+	
+	public State getHomeState() {
+		
+	}
+	
+	public State getNintendoState() {
+		
+	}
+	
+	public State getXBoxState() {
+		
+	}
+	
+	
+	
+}
