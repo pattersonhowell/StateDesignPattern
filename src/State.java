b174292d50a4fb54def5e81a4efa2fcffd25bd5d
Patterson Howell <pattersonhowell@gmail.com>
2020-11-12 15:36:18 -0500

    interface and driver setup

---
+++ src/State.java
@@ -0,0 +1,13 @@
+
+public interface State {
+
+	public void pressHomeButton();
+	
+	public void pressNintendoButton();
+	
+	public void pressXBoxButton();
+	
+	public void pressGameButton();
+	
+	
+}
